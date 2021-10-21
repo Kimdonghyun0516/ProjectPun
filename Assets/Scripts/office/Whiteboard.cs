3d47cfa1395dbb905a7bc781e5eada1d6b212bba
PYJ <dud_wls_95@naver.com>
2021-10-21 21:15:04 +0900

    정리

---
+++ Assets/Scripts/office/Whiteboard.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+
+using System.Linq;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace ChiliGames.VROffice
+{
+    public class Whiteboard : MonoBehaviourPunCallbacks
+    {
+        private int textureSize = 2048;
+        private int penSize = 6;
+        private int penSizeD2 = 3;
+        private Texture2D texture;
+        private Color[] color;
+        private Color[] deleteColor;
+        new Renderer renderer;
+
+        private bool touchingLastFrame;
+        public bool touching;
+
+        private float lastX, lastY;
+        bool everyOthrFrame;
+
+        [HideInInspector] public PhotonView pv;
+        private Texture2D receivedTexture;
+
+        void Start()
+        {
+            renderer = GetComponent<Renderer>();
+            texture = new Texture2D(textureSize, textureSize, TextureFormat.RGBA32, false);
+
+            texture.filterMode = FilterMode.Trilinear;
+            texture.anisoLevel = 3;
+
+            renderer.material.mainTexture = texture;
+
+            Color fillColor = Color.white;
+            var fillColorArray = texture.GetPixels();
+
+            for (var i = 0; i < fillColorArray.Length; ++i)
+            {
+                fillColorArray[i] = fillColor;
+            }
+
+            texture.SetPixels(fillColorArray);
+
+            texture.Apply();
+
+            pv = GetComponent<PhotonView>();
+            Debug.Log("PhotonView = " +pv);
+
+
+            deleteColor = Enumerable.Repeat(Color.white, textureSize * textureSize).ToArray();
+        }
+
+        public void SetPenSize(int n)
+        {
+            penSize = n;
+            penSizeD2 = n / 2;
+        }
+
+        //RPC sent by the Marker class so every user gets the information to draw in whiteboard.
+        [PunRPC]
+        public void DrawAtPosition(float[] pos, int _pensize, float[] _color)
+        {
+            SetPenSize(_pensize);
+            color = SetColor(new Color(_color[0], _color[1], _color[2]));
+
+            int x = (int)(pos[0] * textureSize - penSizeD2);
+            int y = (int)(pos[1] * textureSize - penSizeD2);
+
+            //If last frame was not touching a marker, we don't need to lerp from last pixel coordinate to new, so we set the last coordinates to the new.
+            if (!touchingLastFrame)
+            {
+                lastX = (float)x;
+                lastY = (float)y;
+                touchingLastFrame = true;
+            }
+
+            if (touchingLastFrame)
+            {
+                string str1 = string.Format("setFixel {0}, {1}, {2}, {3}", x, y, penSize, color[0]);
+                Debug.Log(str1);
+
+                texture.SetPixels(x, y, penSize, penSize, color);
+
+                //Lerp last pixel to new pixel, so we draw a continuous line.
+                for (float t = 0.01f; t < 1.00f; t += 0.1f)
+                {
+                    int lerpX = (int)Mathf.Lerp(lastX, (float)x, t);
+                    int lerpY = (int)Mathf.Lerp(lastY, (float)y, t);
+                    texture.SetPixels(lerpX, lerpY, penSize, penSize, color);
+                }
+                //We apply the texture every other frame, so we improve performance.
+                if (!everyOthrFrame)
+                {
+                    everyOthrFrame = true;
+                }
+                else if (everyOthrFrame)
+                {
+                    texture.Apply();
+                    everyOthrFrame = false;
+                }
+            }
+
+            lastX = (float)x;
+            lastY = (float)y;
+        }
+
+        //Reset the state of the whiteboard, so it doesn't interpolate/lerp last pixels drawn.
+        [PunRPC]
+        public void ResetTouch()
+        {
+            touchingLastFrame = false;
+        }
+
+        //Receives the color from the marker
+        public Color[] SetColor(Color color)
+        {
+            return Enumerable.Repeat(color, penSize * penSize).ToArray();
+        }
+
+        //To clear the whiteboard.
+        public void ClearWhiteboard()
+        {
+            pv.RPC("RPC_ClearWhiteboard", RpcTarget.AllBuffered);
+        }
+
+        bool IsLeftMouseButtonDown()
+        {
+#if ENABLE_INPUT_SYSTEM
+            return Mouse.current != null ? Mouse.current.leftButton.isPressed : false;
+#else
+            return Input.GetMouseButtonDown(0);
+#endif
+        }
+
+        [PunRPC]
+        public void RPC_ClearWhiteboard()
+        {
+            texture.SetPixels(deleteColor);
+            texture.Apply();
+        }
+
+        //이 코드는 플라스크 활용하여 할때 가능. 그래서 vr에서는 화이트보드에 기록이 저장이 안된다.
+        
+ /*       public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            base.OnPlayerEnteredRoom(newPlayer);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                pv.RPC("RPC_SendWhiteboard", newPlayer, texture.EncodeToPNG());
+            }
+        }
+
+        [PunRPC]
+        private void RPC_SendWhiteboard(byte[] receivedByte)
+        {
+            receivedTexture = new Texture2D(1, 1);
+            receivedTexture.LoadImage(receivedByte);
+            ApplyReceivedTexture();
+        }
+
+
+        void ApplyReceivedTexture()
+        {
+            GetComponent<Renderer>().material.mainTexture = receivedTexture;
+        }*/
+
+        
+        public void WhiteBoardstart(bool on)
+        {
+            Debug.Log("WhiteBoardstart"+ on);
+
+            Whiting = on;
+        }
+
+        bool Whiting = false;
+
+
+        private void FixedUpdate()
+        {
+
+        }
+    }
+}
